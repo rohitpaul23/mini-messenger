fea67e12f7d6b2fff72e46fa4639f11790e2ef87
Rohit Paul <rohitpaul97@gmail.com>
2022-05-02 21:24:19 +0530

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+const { Router } = require('express');
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "Mini Messageboard", messages: messages });
+});
+
+router.post('/', function(req, res, next) {
+  res.redirect('/new');
+});
+
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: "Add New"});
+});
+
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.message, user: req.body.name, added: new Date()});
+  res.redirect('/');
+});
+
+
+
+module.exports = router;
